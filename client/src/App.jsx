899cb327e755d09f112c8dc6b1da5b1c304ec06e
Siddharth <Siddharthbharti0135@gmail.com>
2026-07-03 23:09:36 +0530

    feat: manage weather data using useState

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,29 @@
 import "./App.css";
+import { useState } from "react";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import WeatherCard from "./components/WeatherCard";
+
+const [weather, setWeather] = useState({
+  city: "Delhi",
+  temperature: 32,
+  condition: "Clear Sky",
+  humidity: 65,
+  wind: 12,
+});
 
 function App() {
   return (
     <div>
       <Header />
       <SearchBar />
-         <WeatherCard
-          city="Delhi"
-          temperature={32}
-          condition="Clear Sky"
-          humidity={65}
-          wind={12}
-          />
+          <WeatherCard
+          city={weather.city}
+          temperature={weather.temperature}
+          condition={weather.condition}
+          humidity={weather.humidity}
+          wind={weather.wind}
+      />
     </div>
   );
 }
