f9dddb35619682d12d4bf9150089d23a9de670c7
Siddharth <Siddharthbharti0135@gmail.com>
2026-07-12 15:04:05 +0530

    Fix CORS middleware

--- server/Index.js
+++ server/Index.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 
+const weatherRoutes = require("./Routes/WeatherRoutes");
+
+const app = express();
+
 app.use(
   cors({
     origin: [
       "https://weather-verse-b79bu3624-weather-now.vercel.app",
       "http://localhost:5173",
     ],
-    methods: ["GET", "POST"],
+    methods: ["GET"],
   })
 );
 
-const weatherRoutes = require("./Routes/WeatherRoutes");
-
-const app = express();
-
-app.use(cors());
 app.use(express.json());
 
 app.use("/api/weather", weatherRoutes);
