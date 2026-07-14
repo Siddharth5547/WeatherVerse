971cba33a372a27072e0c7e20024a189062b2d6d
Siddharth <Siddharthbharti0135@gmail.com>
2026-07-14 15:20:08 +0530

    Update backend CORS configuration

--- client/src/Services/WeatherService.js
+++ client/src/Services/WeatherService.js
@@ -2,14 +2,15 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-// Search by City
-export const fetchWeather = async (city) => {
-  const response = await axios.get(`${API_URL}?city=${city}`);
-  return response.data;
-};
+export const fetchWeather = async (city, lat, lon) => {
+  let url = API_URL;
 
-// Search by GPS Location
-export const fetchWeatherByLocation = async (lat, lon) => {
-  const response = await axios.get(`${API_URL}?lat=${lat}&lon=${lon}`);
-  return response.data;
+  if (city) {
+    url += `?city=${city}`;
+  } else {
+    url += `?lat=${lat}&lon=${lon}`;
+  }
+
+  const res = await axios.get(url);
+  return res.data;
 };
