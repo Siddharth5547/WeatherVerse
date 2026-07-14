87d828687f20b336016345b4f9979f764127ded4
Siddharth <Siddharthbharti0135@gmail.com>
2026-07-14 23:41:10 +0530

    Fix frontend backend API URL

--- client/src/Services/WeatherService.js
+++ client/src/Services/WeatherService.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
-
 export const fetchWeather = async (city) => {
   const response = await axios.get(`${API_URL}?city=${city}`);
   return response.data;
